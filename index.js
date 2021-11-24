0b4911ebc4fc5014441b4f4e40c3c640d7b6d7e8
Wagdi Noman <79422930+Tradepos-co@users.noreply.github.com>
2021-11-24 21:01:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(bodyParser.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/qrc', function(req, res){
+
+    console.log(req.body);
+    /* data = Buffer.concat([data]);
+    body = data.toString();
+    body = JSON.parse(body)
+    rsults = parseData(body); */
+
+    res.end('hi')
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function parseData(data){
+  var name = getTLV(1, data.name);
+  var vat_no = getTLV(2, data.vat_no);
+  var date = getTLV(3, data.date);
+  var total = getTLV(4, data.total);
+  var total_tax = getTLV(5, data.tax);
+  var dataArray = [name, vat_no, date, total, total_tax];
+  return Buffer.concat(dataArray);
+}
+
+function getTLV(tagNum, tagVal) {
+  var bufnum = Buffer.from([tagNum], "utf8");
+  var buflen = Buffer.from([tagVal.length], "utf8");
+  var bufname = Buffer.from(tagVal, "utf8");
+  var bufArray = [bufnum, buflen, bufname];
+  return Buffer.concat(bufArray);
+}
