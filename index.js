b593fd69b53b234210296f225a4f26ea5be894b7
Wagdi Noman <79422930+Tradepos-co@users.noreply.github.com>
2021-12-20 00:51:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ function parseData(data){
 function getTLV(tagNum, tagVal) {
   var bufnum = Buffer.from([tagNum], "utf8");
   var buflen = Buffer.from([tagVal.length], "utf8");
-  var bufname = Buffer.from(tagVal, "utf8");
-  bufname = "H"+bufname;
+  var bufname = Buffer.from("H"+tagVal, "utf8");
   var bufArray = [bufnum, buflen, bufname];
   return Buffer.concat(bufArray);
 }
